7248fbe9a30aa635abe0462360c6e36b4469b587
threezinedine <threezinedine@gmail.com>
2023-02-08 10:50:59 +0700

    add width for login form.

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -15,14 +15,10 @@ import {
 
 
 const LoginPage: React.FC = () => {
-    // todo: the login page using the ant design with form has the username, password
-    // and the login button, the username and password must have more than 8 characters and less than 
-    // 40 characters. the submit button will call the api to the server with the username and password
-    // and the server will return the token and the user information. the token will be stored in the
-    // local storage. 
-
     return (
-        <Form>
+        <Form
+            wrapperCol={{ span: 8 }}
+        >
             <Form.Item
                 label={LOGIN_PAGE_USERNAME_LABEL}
             >
@@ -33,7 +29,7 @@ const LoginPage: React.FC = () => {
             <Form.Item
                 label={LOGIN_PAGE_PASSWORD_LABEL}
             >
-                <Input 
+                <Input.Password 
                     data-testid={LOGIN_PAGE_PASSWORD_DATA_TEST_ID}
                 />
             </Form.Item>
